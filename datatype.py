51de838b4f7b47f4f6d25522b8e1318a1749d05f
prameshdev <rameshparlapalli232@gmail.com>
2021-04-15 15:11:46 +0530

    first comments

---
+++ datatype.py
@@ -0,0 +1,48 @@
+# Text Type:	str
+x = "Ramesh Reddy"
+print(x)
+# Numeric Types:	int, float, complex
+y = 123
+print(y)
+
+z = 10.23
+print(z)
+
+t = 1233j
+print(t)
+# Sequence Types:	list, tuple, range
+someList = ["ON", "OFF"]
+print(someList)
+
+
+someMoreList = ("justIn", "JustOut")
+print(someMoreList)
+
+oneMoreList = range(2)
+print(oneMoreList)
+
+# Mapping Type:	dict
+a = {"name":"ramesh","age":21}
+print(a)
+
+# Set Types:	set, frozenset
+b = {"ramesh","suresh","mahesh"}
+print(b)
+
+c = ({"ramesh","suresh","mahesh"})
+print(c)
+
+
+# Boolean Type:	bool
+d = True
+print(d)
+
+# Binary Types:	bytes, bytearray, memoryview
+e = b"hello"
+print(e)
+
+f = bytearray(3)
+print(f)
+
+g = memoryview(bytes(4))
+print(g)
